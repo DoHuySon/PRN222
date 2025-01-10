e9cb44a31f8f152c3ae88a53f932f3409a37a82d
SonDH <sondhhe170206@fpt.edu.vn>
2025-01-10 09:28:39 +0700

    init repository

---
+++ UDPApp/UDPSereverApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPSereverApp
+{
+    internal class Program
+    {
+        const int listenPort = 11000;
+        const string host = "127.0.0.1";
+        private static void StartListtener()
+        {
+            string message;
+            UdpClient listener = new UdpClient(listenPort);
+            IPAddress adderss = IPAddress.Parse(host);
+            IPEndPoint remoteEndpoint = new IPEndPoint(adderss,listenPort);
+            Console.Title = "UDP Server";
+            Console.WriteLine(new string('*', 40));
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref remoteEndpoint);
+                    message = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+                    Console.WriteLine($"Received broadcasr from {remoteEndpoint} : {message}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread th = new Thread(StartListtener);
+            th.Start();
+        }
+    }
+}
---
+++ firstdatePRN222/FirstdatePRN222/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+
+namespace FirstdatePRN222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uri info = new Uri("https://www.youtube.com/watch?v=FT4tLpLgJuY");
+            //Uri page = new Uri("https://www.facebook.com/stories/134935354871680/UzpfSVNDOjEzMjA3MTE0MTU3ODQ5OTA=/?bucket_count=9&source=story_tray");
+            //Console.WriteLine($"Host: {info.Host}");
+            //Console.WriteLine($"Host: {info.Port}");
+            //Console.WriteLine($"PathAndQuery: {info.PathAndQuery}");
+            //Console.WriteLine($"Query: {info.Query}");
+            //Console.WriteLine($"Fragment: {info.Fragment}");
+            //Console.WriteLine($"Default HTTP port: {page.Port}");
+            //Console.WriteLine($"IsBaseOf: {info.IsBaseOf(page)}");
+            //Uri relative = info.MakeRelativeUri( page );
+            //Console.WriteLine($"IsAbsoluteUri: {relative.IsAbsoluteUri}");
+            //Console.WriteLine($"RelativeUri: {relative.ToString()}");
+            //Console.ReadLine();
+
+
+            WebRequest request = WebRequest.Create("https://www.facebook.com/Gaixinhpagekhongthieu");
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine("Status: " + response.StatusDescription);
+            Console.WriteLine(new String('*', 50));
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            Console.WriteLine(new String('*', 50));
+            reader.Close();
+            stream.Close();
+            response.Close();
+        }
+    }
+}
