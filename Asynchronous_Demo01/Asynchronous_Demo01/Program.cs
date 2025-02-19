a5029e43630a102c4d0be779094349fb575758e2
SonDH <sondhhe170206@fpt.edu.vn>
2025-02-19 10:02:38 +0700

    slot3

---
+++ Asynchronous_Demo01/Asynchronous_Demo01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace Asynchronous_Demo01
+{
+    internal class Program
+    {
+        private static void DownloadAsynchronously()
+        {
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted +=
+                new DownloadStringCompletedEventHandler(DownloadComplete);
+            client.DownloadStringAsync(new Uri("http://www.aspnet.com"));
+        }
+
+        // --------------------------------------------------------------------
+        private static void DownloadComplete(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                Console.WriteLine("Some error has occurred.");
+                return;
+            }
+
+            // Print result
+            Console.WriteLine(e.Result);
+            Console.WriteLine(new string('*', 30));
+            Console.WriteLine("Download completed.");
+        }
+        static void Main(string[] args)
+        {
+            DownloadAsynchronously();
+            Console.WriteLine("Main thread : Done");
+            Console.WriteLine(new string('*', 30));
+            Console.ReadLine();
+        }
+    }
+}
